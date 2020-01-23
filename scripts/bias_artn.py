81b1f329a70bee38b8da9bf6d35cae16fdf6744e
David Sand <dave.j.sand@gmail.com>
2020-01-22 22:24:03 -0700

    made bias_artn work

--- scripts/bias_artn.py
+++ scripts/bias_artn.py
@@ -1,13 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-from rts2.bias_DS import Bias, BiasScript
-import rts2.scriptcomm
+from rts2solib import scriptcomm
 import json
 #from rts2solib.rts2_wwwapi import rts2comm
 from rts2solib.big61filters import filter_set; Filters=filter_set()
+from rts2solib import to_dataserver
 import numpy
 from telescope import kuiper;k=kuiper()
+    
 
-f = BiasScript(numbias=1,binning=2)
+#class BiasScript (scriptcomm.Rts2Comm):
+#
+#    """Class for taking a bunch of biases."""
+#
+#
+#    def __init__(
+#            self, numbias=2, binning=3 ):
+#        scriptcomm.Rts2Comm.__init__(self)
+#
+#        self.numbias = numbias
+#        self.binning = binning
+#
+#    def takeBias(self):
+#        self.setValue('SHUTTER', 'DARK')
+#        self.setValue('exposure', 0)
+#        filename = self.exposure(None, "yo.fits")
+#        
+#        to_dataserver(filename, "yo.fits")
+#        i = 0
+##        while i < self.numbias:
+##            i += 1
+##            bias = self.exposure()
+##            self.toDark(bias)
+#
+#
+#    def run(self):
+#        self.takeBias()
+ 
+script = scriptcomm.Rts2Comm()
+script.setValue('SHUTTER', 'LIGHT')
+script.setValue('exposure', 0)
+script.setValue('binning',3)
 
-f.run()
+filename = script.exposure(None, "thisisatest.fits")
+
+to_dataserver(filename, "daveisaprogrammer.fits")
+
